2dae40a3954e3d5f3f602a92634f833eec3b42fe
KorAidenChoi <kor.aiden.choi@gmail.com>
2023-04-26 20:28:33 -0700

    finally got the image working

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 import 'package:webtoon/services/api_service.dart';
+import 'dart:async';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -27,22 +28,86 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        Image thumb = Image.network(
+          webtoon.thumb,
+          errorBuilder: (context, error, stackTrace) {
+            return const Text("An error has occured with this image.");
+          },
+        );
+        thumb.image
+            .resolve(ImageConfiguration.empty)
+            .addListener(ImageStreamListener((_, __) {
+          onError:
+          (_, __) {
+            imageCache.evict(thumb.image);
+          };
+        }));
+        WidgetsFlutterBinding.ensureInitialized();
+        FlutterError.onError = (details) {
+          if (details.exception is! NetworkImageLoadException) {
+            throw details.exception;
+          }
+        };
+        Future(() => print('future 1'));
+        Future(() => print('future 2'));
+        // Microtasks will be executed before futures.
+        Future.microtask(() => print('microtask 1'));
+        Future.microtask(() => print('microtask 2'));
+
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(5, 10),
+                      color: Colors.black.withOpacity(1),
+                    )
+                  ]),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
